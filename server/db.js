bf9fb54d9ebd30d508d205ea98c4e27bfe76b7ae
Hill Kim <hillkim7@gmail.com>
2018-02-10 09:01:30 +0900

    Make Mongodb reconnect
    On Ubuntu Linux 16.04, it sometimes fails to establish mongodb connection with production run mode(NODE_ENV=production).
    This fix will make it retry a connection for this case.

--- server/db.js
+++ server/db.js
@@ -6,12 +6,12 @@ var debug = require('debug')('meanstackjs:db')
 var glob = require('glob')
 var _ = require('lodash')
 var path = require('path')
+var disconnectedBy = false
 
 function mongoDB (self) {
   // Connect to MongoDb
   mongoose.Promise = global.Promise
   mongoose.set('debug', self.settings.mongodb.debug)
-  mongoose.connect(self.settings.mongodb.uri, self.settings.mongodb.options)
   mongoose.connection.on('error', function (error) {
     self.logger.warn('MongoDB Connection Error. Please make sure that MongoDB is running.')
     debug('MongoDB Connection Error ', error)
@@ -19,6 +19,16 @@ function mongoDB (self) {
   mongoose.connection.on('open', function () {
     debug('MongoDB Connection Open ')
   })
+  mongoose.connection.on('reconnected', function () {
+    debug('MongoDB reconnected!')
+  })
+  mongoose.connection.on('disconnected', function () {
+    if (!disconnectedBy) {
+      debug('MongoDB disconnected! -- connect again')
+      setTimeout(function (){ mongoose.connect(self.settings.mongodb.uri, self.settings.mongodb.options) }, 5000)
+    }
+  })
+  mongoose.connect(self.settings.mongodb.uri, self.settings.mongodb.options)
   // Register All Mongoose Models
   self.models = {}
   var files = glob.sync('server/modules/**/*.model.js')
@@ -33,5 +43,6 @@ function mongoDB (self) {
 }
 function disconnect (cb) {
   if (!cb)cb = function () {}
+  disconnectedBy = True
   mongoose.disconnect(cb)
 }
